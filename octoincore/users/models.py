d5d9f19add31364062653701f65d91108a47ad87
0x70 <greezygreed@gmail.com>
2023-03-10 22:54:43 +0100

    user products sold this month count

--- octoincore/users/models.py
+++ octoincore/users/models.py
@@ -3,11 +3,25 @@ from email.policy import default
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from octoincore.payments.models import OrderInvoice
+
 
 class ExtendUser(AbstractUser):
 
     email = models.EmailField(blank=False, max_length=255, verbose_name="email")
-    profile_image = models.ImageField(upload_to="uploads/profile_images/", blank=True, null=True, default="defaults/profile_image.png")
+    profile_image = models.ImageField(
+        upload_to="uploads/profile_images/",
+        blank=True,
+        null=True,
+        default="defaults/profile_image.png",
+    )
+
+    def products_sold_this_month_count(self):
+        return OrderInvoice.objects.filter(
+            order__basket__basket_objects__product_inventory__product__owner=self,
+            order__created_at__month=1,
+            paid=True,
+        ).count()
 
     USERNAME_FIELD = "username"
     EMAIL_FIELD = "email"
--- octoincore/users/schema.py
+++ octoincore/users/schema.py
@@ -1,9 +1,18 @@
 import datetime
-import profile
+import typing
 from typing import List, Optional
 
 import strawberry
 import strawberry_django_jwt.mutations as jwt_mutations
+from django.db.models import Count
+
+from .models import ExtendUser
+
+# from octoincore.inventory.schema import ProductType
+
+
+if typing.TYPE_CHECKING:
+    from octoincore.inventory.schema import ProductType
 
 
 @strawberry.type
@@ -18,6 +27,20 @@ class MeType:
     dateJoined: datetime.datetime
     lastLogin: datetime.datetime
     profileImage: str
+
+
+@strawberry.django.type(model=ExtendUser)
+class UserType:
+    username: str
+    isStaff: bool
+    isActive: bool
+    isSuperuser: bool
+    dateJoined: datetime.datetime
+    profileImage: str
+    products: typing.List[
+        typing.Annotated["ProductType", strawberry.lazy("octoincore.inventory.schema")]
+    ]
+
 
 @strawberry.type
 class UserQuery:
@@ -34,9 +57,37 @@ class UserQuery:
                 firstName=info.context.request.user.first_name,
                 dateJoined=info.context.request.user.date_joined,
                 lastLogin=info.context.request.user.last_login,
-                profileImage=info.context.request.build_absolute_uri(info.context.request.user.profile_image.url),
+                profileImage=info.context.request.build_absolute_uri(
+                    info.context.request.user.profile_image.url
+                ),
             )
         return None
+
+    @strawberry.field
+    def top_20_users(self, info) -> List[UserType]:
+        top_users = sorted(
+            ExtendUser.objects.all(),
+            key=lambda x: x.products_sold_this_month_count(),
+            reverse=True,
+        )[:20]
+
+        top_user_types = []
+        for user in top_users:
+            top_user_types.append(
+                UserType(
+                    username=user.username,
+                    isStaff=user.is_staff,
+                    isActive=user.is_active,
+                    isSuperuser=user.is_superuser,
+                    dateJoined=user.date_joined,
+                    profileImage=info.context.request.build_absolute_uri(
+                        user.profile_image.url
+                    ),
+                    products=user.products.all(),
+                )
+            )
+        return top_user_types
+
 
 @strawberry.type
 class UserMutations:
@@ -44,4 +95,3 @@ class UserMutations:
     verify_token = jwt_mutations.Verify.verify
     refresh_token = jwt_mutations.Refresh.refresh
     delete_token_cookie = jwt_mutations.DeleteJSONWebTokenCookie.delete_cookie
-
