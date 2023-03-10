41c39514ce8de324c726406bea32c659d24c859a
0x70 <greezygreed@gmail.com>
2023-03-10 22:56:50 +0100

    config replaced by better env

--- octoincore/config/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class ServerSettings(models.Model):
-    attribute = models.CharField(max_length=4095)
-    value = models.CharField(max_length=4095)
--- octoincore/config/on_setup.py
+++
@@ -1,8 +0,0 @@
-from .models import ServerSettings
-
-default_settings = {
-    ""
-}
-
-for attribute in default_settings:
-    ServerSettings.objects.create(attribute=attribute, value=default_settings[attribute])
